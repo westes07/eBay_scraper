00875ebf696c453fc61e156fa4699c5eb2072449
Corbin <33209092+westes07@users.noreply.github.com>
2020-07-19 19:43:56 -0600

    Remove get api key as serverless. Added to keyword search file.

--- functions/keyword_search.js
+++ functions/keyword_search.js
@@ -1,8 +1,31 @@
-exports.handler = function(event, context, callback) {
-    fetch("https://ebayscraper.netlify.app/.netlify/functions/get_api_key")
+function get_api_key(){
+    let key = "Error: No data received";
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic Q29yYmluRXMtQnV5LVBSRC1iYzhlZTU1NzYtOGZiMTRiZGM6UFJELWM4ZWU1NTc2NWZmYi05MTIyLTQxYzMtYThmYi1lNDUw");
+    myHeaders.append("Cookie", "ebay=%5Esbf%3D%23%5E; dp1=bu1p/QEBfX0BAX19AQA**62d391b8^");
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "client_credentials");
+    urlencoded.append("scope", "https://api.ebay.com/oauth/api_scope");
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.ebay.com/identity/v1/oauth2/token", requestOptions)
         .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(err => console.log('Error', err));
+        .then(result => key = result)
+        .catch(error => console.log('error', error));
+
+    process.env.apiKey = key;
+}
+
+exports.handler = function(event, context, callback) {
+    get_api_key();
 
     let apiKey = process.env.apiKey;
     let search_result;
